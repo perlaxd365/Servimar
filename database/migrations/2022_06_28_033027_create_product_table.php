be089c48e12f25c8c51bbe14a9a7e58d5d6e6c3c
Perla <perlaxd365@gmail.com>
2022-06-28 01:11:48 -0500

    subida 28062022

---
+++ database/migrations/2022_06_28_033027_create_product_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->bigIncrements('id_producto');
+            $table->unsignedBigInteger('id_sede')->nullable()->comment('id de la sede en donde se ubicara el producto');
+            $table->string('nombre')->nullable()->comment('dni del cliente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+};
